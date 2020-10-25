75bf788c9f0aa24aedae1f6502879ec93beca86a
Ghoster23 <mmpcp23@gmail.com>
2020-10-25 19:55:36 +0000

    Preliminary Block Stuff

---
+++ Assets/Scripts/ProgrammingBlock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgrammingBlock : MonoBehaviour
+{
+    protected Transform Port;
+
+    protected float track;
+
+    [SerializeField]
+    float id;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.track = 0;
+
+        this.Port = CheckPort();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected Transform CheckPort()
+    {
+        return this.transform.Find("Port");
+    }
+}
