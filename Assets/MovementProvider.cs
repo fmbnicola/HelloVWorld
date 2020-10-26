de92208a1595d16301b7e507a88b417228c8992e
Fmbnicolau <fmbnicolau@hotmail.com>
2020-10-26 23:57:19 +0000

    Locomotion base

--- Assets/MovementProvider.cs
+++ Assets/MovementProvider.cs
@@ -1,18 +1,99 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class MovementProvider : MonoBehaviour
+
+
+public class MovementProvider : LocomotionProvider
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 1.0f;
+    public float gravityMultiplier = 1.0f;
+    public List<XRController> controllers = null;
+
+    private CharacterController characterController = null;
+    private GameObject head = null;
+
+    protected override void Awake()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        head = GetComponent<XRRig>().cameraGameObject;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        PositionController();
+    }
+
+    private void FixedUpdate()
+    {
+        PositionController();
+        CheckForInput();
+        ApplyGravity();
+    }
+
+    //Move player capsule with camera
+    private void PositionController()
+    {
+        // Get the head in local, playspace ground
+        float headHeight = Mathf.Clamp(head.transform.localPosition.y, 1, 2);
+        characterController.height = headHeight;
+
+        // Cut in half, add skin width
+        Vector3 newCenter = Vector3.zero;
+        newCenter.y = characterController.height / 2;
+        newCenter.y += characterController.skinWidth;
+
+        //Let's move the capsule in local space as well
+        newCenter.x = head.transform.localPosition.x;
+        newCenter.z = head.transform.localPosition.z;
+
+        //Apply
+        characterController.center = newCenter;
+    }
+
+    private void CheckForInput()
+    {
+        foreach(XRController controller in controllers)
+        {
+            if (controller.enableInputActions)
+            {
+                CheckForMovement(controller.inputDevice);
+            }
+        }
+    }
+
+    private void CheckForMovement(InputDevice device)
+    {
+        if(device.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 position))
+        {
+            StartMove(position);
+        }
+    }
+
+    private void StartMove(Vector2 position)
+    {
+        // Apply the touch postion to the head's forward Vector
+        Vector3 direction = new Vector3(position.x, 0, position.y);
+        Vector3 headRotation = new Vector3(0, head.transform.eulerAngles.y, 0);
+
+        // Rotate the input direction by the horizontal head rotation
+        direction = Quaternion.Euler(headRotation) * direction;
+
+        // Apply speed and move
+        Vector3 movement = direction * speed;
+        characterController.Move(movement *Time.fixedDeltaTime);
+    }
+
+    private void ApplyGravity()
+    {
+        if (!characterController.isGrounded)
+        {
+            Vector3 gravity = new Vector3(0, Physics.gravity.y * gravityMultiplier, 0);
+            gravity.y *= Mathf.Pow(Time.fixedDeltaTime, 2);
+
+            characterController.Move(gravity);
+        }
     }
 }
