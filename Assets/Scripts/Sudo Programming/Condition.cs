97c9c1928b188aacb485245d74b3afe2e02dd204
Ghoster23 <mmpcp23@gmail.com>
2020-10-26 18:32:23 +0000

    Sudo Programming 0.1
    Added classes for several programming elements.

---
+++ Assets/Scripts/Sudo Programming/Condition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Condition
+{
+    public Sensor Sensor { get; protected set; }
+    public Value Value { get; protected set; }
+    public Comparator Comparator { get; protected set; }
+
+
+    Condition(Sensor sensor, Value value, Comparator comparator)
+    {
+        this.Sensor = sensor;
+        this.Value = value;
+        this.Comparator = comparator;
+    }
+
+
+    public bool Check(Transform robot) // Needs to be changed to the MonoBehaviour class
+    {
+        var sensed = this.Sensor.GetValue(robot);
+
+        return this.Comparator.Compare(sensed, this.Value);
+    }
+}
---
+++ Assets/Scripts/Sudo Programming/Sensor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sensor
+{
+    public enum ID
+    {
+        Camera,
+        Microphone
+    }
+
+    public ID Id { get; protected set; }
+
+    Sensor(ID id)
+    {
+        this.Id = id;
+    }
+
+
+    public Value GetValue(Transform robot) // Change to the MonoBehaviour class
+    {
+        // return robot.Sense(this.Id);
+
+        return null;
+    }
+}
