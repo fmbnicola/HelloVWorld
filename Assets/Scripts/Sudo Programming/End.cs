a040bcfec3e5b17dfc734c5fba4903c261bdf576
Ghoster23 <mmpcp23@gmail.com>
2020-10-28 15:18:03 +0000

    New Scene for Manel/Cris

---
+++ Assets/Scripts/Sudo Programming/End.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End : CodeNode
+{
+    public End(CodeNode context, CodeNode prev) : base(context, prev) { }
+}
