029dc57609ea2ed5380de0281d2cdca59acdea15
Miguel <miguel.valerio@tecnico.ulisboa.pt>
2020-10-28 12:32:29 +0000

    methods to execute program

--- Assets/Scripts/Robot/RobotController.cs
+++ Assets/Scripts/Robot/RobotController.cs
@@ -8,6 +8,12 @@ namespace Robot
 {
     public class RobotController : MonoBehaviour
     {
+        // TODO: change to correct type
+        private List<Object> Program { get; set; }
+
+
+
+        #region === Unity Events ===
 
         // Start is called before the first frame update
         void Start()
@@ -15,11 +21,34 @@ namespace Robot
 
         }
 
+
         // Update is called once per frame
         void Update()
         {
 
         }
-    }
 
+        #endregion
+
+
+
+        #region === Program Methods ===
+
+        public void LoadProgram(List<Object> newProgram)
+        {
+            this.Program = newProgram;
+        }
+
+
+        public void ExecuteProgram()
+        {
+            foreach(Object instruction in this.Program)
+            {
+                // this.ActionController.Execute(instruction);
+            }
+        }
+
+        #endregion
+
+    }
 }
