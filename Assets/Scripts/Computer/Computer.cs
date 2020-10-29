1bf9aac9243c0c8f7aa2e93f16ae7e281aa59d8f
Ghoster23 <mmpcp23@gmail.com>
2020-10-29 18:50:39 +0000

    Manel things

--- Assets/Scripts/Computer/Computer.cs
+++ Assets/Scripts/Computer/Computer.cs
@@ -6,17 +6,63 @@ public class Computer : MonoBehaviour
 {
     public ProgrammingBlock StartBlock;
 
+    public enum States
+    {
+        Idle,
+        StartUp,
+        Active
+    }
+
+    public States State { get; private set; }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.State = States.Idle;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch(this.State)
+        {
+            default:
+                break;
+
+            case States.StartUp:
+                break;
+
+            case States.Active:
+                break;
+        }
+    }
+
+
+    public void StartUp()
+    {
+        if (this.State == States.Idle) this.State = States.StartUp;
+    }
+
+    public void Save()
+    {
+        var program = this.Parse();
+
+        //Send to Floppy
+    }
+
+    public void Clear()
+    {
+        //Clear Block Links
+    }
+
+    public void ShutDown()
+    {
+        if(this.State == States.Active)
+        {
+            this.Clear();
+
+            this.State = States.Idle;
+        }
     }
 
 
---
+++ Assets/Scripts/Computer/Dais.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dais : MonoBehaviour
+{
+    private Computer Computer;
+
+    public Computer.States State { get; private set; }
+
+    private float AntiGravityForce = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Computer = this.transform.parent.GetComponent<Computer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.State = this.Computer.State;
+    }
+
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.attachedRigidbody == null || collider.attachedRigidbody.mass >= 10f) return;
+
+        switch(this.State)
+        {
+            default:
+                break;
+
+            case Computer.States.StartUp: //Make them lift off the ground
+                if(collider.attachedRigidbody.velocity.y < 5)
+                    collider.attachedRigidbody.AddForce(new Vector3(0,6,0));
+                break;
+
+            case Computer.States.Active: // Turn off their gravity
+                collider.attachedRigidbody.useGravity  = false;
+                collider.attachedRigidbody.isKinematic = true;
+                break;
+        }
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.attachedRigidbody == null || collider.attachedRigidbody.mass >= 10f) return;
+
+        switch (this.State)
+        {
+            default:
+                break;
+
+            case Computer.States.StartUp: //Make them lift off the ground
+                if (collider.attachedRigidbody.velocity.y < 5)
+                    collider.attachedRigidbody.AddForce(new Vector3(0, 6, 0));
+                break;
+
+            case Computer.States.Idle:
+                collider.attachedRigidbody.useGravity  = true;
+                collider.attachedRigidbody.isKinematic = false;
+                break;
+        }
+
+        if (collider.gameObject.name == "StartBlock")
+        {
+            this.Computer.StartBlock = collider.gameObject.GetComponent<ProgrammingBlock>();
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.attachedRigidbody == null || collider.attachedRigidbody.mass >= 10f) return;
+
+        if (this.Computer.StartBlock != null && collider.gameObject == this.Computer.StartBlock)
+            this.Computer.StartBlock = null;
+
+        // Make sure Gravity and Kinematic are right
+         collider.attachedRigidbody.useGravity  = true;
+         collider.attachedRigidbody.isKinematic = false;
+    }
+}
