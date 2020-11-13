e6ef972c62a4e9318ea414696b08737d87a118f4
Fmbnicolau <fmbnicolau@hotmail.com>
2020-11-13 01:45:30 +0000

    ProgrammingBlocks symbols (auto symbol swap in editor)

--- Assets/Scripts/Programming Blocks/InstructionBlock.cs
+++ Assets/Scripts/Programming Blocks/InstructionBlock.cs
@@ -7,7 +7,6 @@ public class InstructionBlock : ProgrammingBlock
     [SerializeField]
     private Instruction.ID Id = Instruction.ID.Drop;
 
-
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +17,13 @@ public class InstructionBlock : ProgrammingBlock
     void Update()
     {
         
+    }
+
+    private void OnValidate()
+    {
+        var symbol = transform.Find("Symbol");
+        var renderer = symbol.GetComponent<Renderer>();
+        renderer.sharedMaterial.SetInt("_Instruction", (int) Id);
     }
 
 
