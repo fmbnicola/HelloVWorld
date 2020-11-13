95cd7a80db7bb4d13cdaa676d121cd0af35904eb
Fmbnicolau <fmbnicolau@hotmail.com>
2020-11-13 00:49:40 +0000

    New textures + robo animatior interface

--- Assets/Scripts/Robot/RobotAnimationController.cs
+++ Assets/Scripts/Robot/RobotAnimationController.cs
@@ -4,15 +4,82 @@ using UnityEngine;
 
 public class RobotAnimationController : MonoBehaviour
 {
+    public GameObject threads = null;
+    public float speed = 0.3f;
+
+    private Renderer ThreadRenderer;
+    private Material ThreadMaterial;
+
+    enum FaceExpression
+    {
+        Normal,
+        Excited,
+        Mad,
+        Sad
+    }
+
+    public GameObject face = null;
+    public int expression = 0;
+
+    private Renderer FaceRenderer;
+    private Material FaceMaterial;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ThreadRenderer = threads.GetComponent<Renderer>();
+        List<Material> mats = new List<Material>();
+        ThreadRenderer.GetMaterials(mats);
+
+        ThreadMaterial = mats[1];
+
+        FaceRenderer = face.GetComponent<Renderer>();
+        FaceMaterial = FaceRenderer.material;
     }
 
-    // Update is called once per frame
-    void Update()
+    // Set threads to moving foward
+    void ThreadsFoward()
     {
-        
+        ThreadMaterial.SetFloat("_Speed", speed);
+    }
+
+    // Set threads to moving in reverse
+    void ThreadsReverse()
+    {
+        ThreadMaterial.SetFloat("_Speed", -speed);
+    }
+
+    // Set threads to stop moving
+    void ThreadsStop()
+    {
+        ThreadMaterial.SetFloat("_Speed", 0);
+    }
+
+    // Make sad face
+    void FaceSad(float expressionTime = 1.0f)
+    {
+        FaceMaterial.SetInt("_Face", (int) FaceExpression.Sad);
+        StartCoroutine(ResetFace(expressionTime));
+    }
+
+    // Make excited face
+    void FaceExcited(float expressionTime = 1.0f)
+    {
+        FaceMaterial.SetInt("_Face", (int)FaceExpression.Excited);
+        StartCoroutine(ResetFace(expressionTime));
+    }
+
+    // Make mad face
+    void FaceMad(float expressionTime = 1.0f)
+    {
+        FaceMaterial.SetInt("_Face", (int)FaceExpression.Mad);
+        StartCoroutine(ResetFace(expressionTime));
+    }
+
+    private IEnumerator ResetFace(float time)
+    {
+        yield return new WaitForSeconds(time);
+        FaceMaterial.SetInt("_Face", (int) FaceExpression.Normal);
+        yield break;
     }
 }
