ec21168c25d3aadbbe9ab5f33fb030e35c16ccd3
Miguel <miguel.valerio@tecnico.ulisboa.pt>
2020-11-13 11:56:46 +0000

    start tile v1

---
+++ Assets/Scripts/Puzzle/StartTile.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace Puzzle
+{
+    public class StartTile : MonoBehaviour
+    {
+        #region /* Set in Editor */
+
+        public Vector3 Rotation;
+
+        public string RobotTag;
+
+        #endregion
+
+
+
+        #region === Unity Events ===
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.CompareTag(this.RobotTag))
+            {
+                Debug.Log("robot detected");
+            }
+        }
+
+        #endregion
+    }
+}
