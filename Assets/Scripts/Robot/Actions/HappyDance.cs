e54d3b3f70d2eae25754eba3c2bf848b62cdacd0
Miguel <miguel.valerio@tecnico.ulisboa.pt>
2020-11-15 21:38:31 +0000

    happy dance now is an action

---
+++ Assets/Scripts/Robot/Actions/HappyDance.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace Robot.Actions
+{
+    public class HappyDance : Action
+    {
+        public HappyDance(RobotController robot, End end) : base(robot, end) { }
+
+
+
+        public override void Execute()
+        {
+            if (this.Robot.DebugInfo)
+            {
+                Debug.Log(this.ProgramLine.ToString() + " -> " + this.ToString());
+            }
+
+            this.ProgramLine.Next = ProgramHelper.HappyDance();
+
+            this.ProgramLine.Complete = true;
+        }
+
+    }
+}
